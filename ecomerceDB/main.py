8bd49f56ce9d9a8f1df722f364561d373730bc03
cartertheesfeld <111896080+cartertheesfeld@users.noreply.github.com>
2023-04-11 15:50:18 -0500

    Update main.py

--- ecomerceDB/main.py
+++ ecomerceDB/main.py
@@ -8,9 +8,13 @@ db = sql.SQLServer(server, database)
 
 db.connect()
 
+itemGenerator = ig.ItemGen()
+
 # Create item data
-itemData = ig.CreateItemData()
+itemData = itemGenerator.CreateItemData()
 
 # Print out the item data
 print(itemData)
-
+for x in range(10000):
+    itemData = itemGenerator.CreateItemData()
+    db.send_data('items', itemData, columns=["itemName", "decription", "price"])
