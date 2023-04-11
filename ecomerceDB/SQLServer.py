d66d1182c07fe51e858cd2dbbec6daec35f2a2cc
cartertheesfeld <111896080+cartertheesfeld@users.noreply.github.com>
2023-04-11 15:48:58 -0500

    Update SQLServer.py

--- ecomerceDB/SQLServer.py
+++ ecomerceDB/SQLServer.py
@@ -1,40 +1,40 @@
 import pyodbc
 
-
 class SQLServer:
-    def __init__(self, server, database):
+    def __init__(self, server, database, trusted_connection=True):
         self.server = server
         self.database = database
-        self.connection = None
+        self.trusted_connection = trusted_connection
+        self.conn = None
         self.cursor = None
-
+    
     def connect(self):
-        try:
-            self.connection = pyodbc.connect(f"DRIVER={{SQL Server}};SERVER={self.server};DATABASE={self.database};Trusted_Connection=yes")
-            self.cursor = self.connection.cursor()
-            print("Connection established successfully!")
-        except Exception as e:
-            print(f"Error connecting to SQL Server: {e}")
+        if self.trusted_connection:
+            self.conn = pyodbc.connect(
+                f"Driver={{ODBC Driver 17 for SQL Server}};Server={self.server};Database={self.database};Trusted_Connection=yes;"
+            )
+        else:
+            raise ValueError("Not implemented: Non-trusted connection")
 
+        self.cursor = self.conn.cursor()
+    
     def disconnect(self):
-        try:
-            self.connection.close()
-            print("Connection closed successfully!")
-        except Exception as e:
-            print(f"Error disconnecting from SQL Server: {e}")
-
+        self.conn.close()
+    
     def execute_query(self, query):
-        try:
-            self.cursor.execute(query)
-            rows = self.cursor.fetchall()
-            return rows
-        except Exception as e:
-            print(f"Error executing query: {e}")
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        return rows
+    
+    def send_data(self, table_name, data, columns=None):
+        if columns is None:
+            query = f"INSERT INTO {table_name} VALUES ({','.join(['?' for _ in range(len(data[0]))])})"
+        else:
+            query = f"INSERT INTO {table_name} ({','.join(columns)}) VALUES ({','.join(['?' for _ in range(len(data[0]))])})"
 
-    def insert_data(self, table_name, data):
-        cursor = self.cnxn.cursor()
-        columns = ", ".join(data.keys())
-        values = ", ".join("?" * len(data))
-        query = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
-        cursor.execute(query, tuple(data.values()))
-        self.cnxn.commit()
+        self.cursor.executemany(query, data)
+        self.conn.commit()
+    
+    def query_table(self, table_name):
+        query = f"SELECT * FROM {table_name}"
+        return self.execute_query(query)
